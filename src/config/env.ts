eb9815ba39f725d6e8a507ae39ba2650fa589e3c
moisas <moihgm67@gmail.com>
2026-09-26 19:13:10 -0300

    feat: add about and support page

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,9 @@ const Environment = {
     VITE_API_GEOAPIFY: import.meta.env.VITE_API_GEOAPIFY,
     VITE_API_GEOAPIFY_KEY: import.meta.env.VITE_API_GEOAPIFY_KEY,
     VITE_BACKEND_URL: import.meta.env.VITE_BACKEND_URL,
+    VITE_SUPPORT_URL: import.meta.env.VITE_SUPPORT_URL,
+    VITE_SUPPORT_PIX: import.meta.env.VITE_SUPPORT_PIX,
+    VITE_CONTACT_EMAIL: import.meta.env.VITE_CONTACT_EMAIL,
 }
 
 export default Environment;
